eca51a7b0579aad97da856b43afdb2e50e1eee48
Олег <oleg.bolotov97@gmail.com>
2026-06-30 13:47:31 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+task-manager-api/
+│── app.py          # Основной файл Flask
+│── models.py       # ORM-модели SQLAlchemy
+│── database.py     # Подключение к базе
+│── requirements.txt # Зависимости
+│── README.md        # Описание проекта
+│── .gitignore       # Исключения
+
+tasks.db            # База данных (можно не добавлять)
+.env.example        # Пример переменных окружения
+tests/              # Тесты
+Dockerfile          # Контейнеризация
+LICENSE             # Лицензия MIT
